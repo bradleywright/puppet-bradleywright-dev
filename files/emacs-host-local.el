291c71920cdfdbbda14abe16b49ccc4f60ac0dd4
Bradley Wright <brad@intranation.com>
2013-08-24 23:02:12 +0100

    Make sure the Emacs client is right

--- files/emacs-host-local.el
+++ files/emacs-host-local.el
@@ -1,2 +1,4 @@
 (exec-path-from-shell-copy-envs '("BOXEN_NVM_DIR"
                                   "BOXEN_NVM_DEFAULT_VERSION"))
+;; make sure Magit knows where to find the Emacs client
+(setq magit-emacsclient-executable "/opt/boxen/homebrew/bin/emacsclient")
